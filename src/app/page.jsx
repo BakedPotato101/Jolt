6ea325dfb3eb672812bc8a362ce21448357478a2
natanojjj <chavvas.johnny@gmail.com>
2024-04-13 13:42:47 -0500

    corrected home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="bg-white dark:bg-slate-900 w-full h-full pt-20 flex flex-col items-center justify-center dark:text-white" style={{ backgroundImage: 'url("OIP (2).jpeg")', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+    <div className="bg-gradient-to-br from-tan to-orange dark:bg-slate-900 w-full h-full pt-20 flex flex-col items-center justify-center dark:text-white" style={{ backgroundImage: 'linear-gradient(to bottom right, #FCD6A4, #FFA07A)'}}>
       <h1 className="text-7xl font-bold text-center mb-4">Welcome to Jolt</h1>
       <p className="text-lg text-center font-bold mb-8">Lightning Fast Stats</p>
       {/* Search bar */}
