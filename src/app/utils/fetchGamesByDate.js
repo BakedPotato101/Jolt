837d044eb74699cb2d826d398bdd8aa56b0bbcd3
TristanJ <TristanJones11.TJ@gmail.com>
2024-04-13 05:58:44 -0500

    WIP Games

---
+++ src/app/utils/fetchGamesByDate.js
@@ -0,0 +1,21 @@
+export default async function fetchGamesById(id) {
+    const response = await fetch('https://v1.american-football.api-sports.io/games?date=' + date, {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': process.env.API_KEY,
+        'x-rapidapi-host': 'v1.american-football.api-sports.io',
+      },
+    });
+    console.log(response)
+  
+    if (!response.ok) {
+      throw new Error('Failed to fetch game');
+    }
+  
+    const data = await response.json();
+    if (data.response == []) {
+      throw new Error(data.errors[0].message);
+    }
+    return data.response;
+  }
+  
