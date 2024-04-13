b7aca0b5188abb399e9458aab65c6e5210e27032
BakedPotato101 <jackson.baker7562@gmail.com>
2024-04-13 07:14:42 -0500

    fixed sidebar icons and added to games

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,13 +1,13 @@
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faUsers, faUserTie, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faUsers, faFootball, faUser } from '@fortawesome/free-solid-svg-icons';
 export default function Sidebar() {
   return (
     <div className="bg-slate-700 h-screen w-20 flex-row items-center justify-normal">
       <Link href="/"><FontAwesomeIcon className="pt-40 w-full size-12 text-white" icon={ faHome }/></Link>
-      <Link href="/players"><FontAwesomeIcon className="pt-40 w-full size-12 text-white" icon={ faUser}/></Link>
-      <Link href="/teams"><FontAwesomeIcon className="pt-40 w-full size-12 text-white" icon={ faUsers}/></Link>
-      <Link href="/coaches"><FontAwesomeIcon className="pt-40 w-full size-12 text-white" icon={ faUserTie}/></Link>
+      <Link href="/players"><FontAwesomeIcon className="pt-40 w-full size-12 text-white" icon={ faUser }/></Link>
+      <Link href="/teams"><FontAwesomeIcon className="pt-40 w-full size-12 text-white" icon={ faUsers }/></Link>
+      <Link href="/games"><FontAwesomeIcon className="pt-40 w-full size-12 text-white" icon={ faFootball }/></Link>
     </div>
   );
 }
--- src/app/games/page.jsx
+++ src/app/games/page.jsx
@@ -1,24 +1,42 @@
-import { fetchGames } from "../utils/fetchGames"; 
-import Link from 'next/link';
+import { fetchGames } from "../utils/fetchGames";
+import Link from "next/link";
 
 export default async function Page() {
-  const gameData = await fetchGames(); 
+  const gameData = await fetchGames();
   console.log("fetched games", gameData);
 
   return (
     <div className="pt-20 space-y-4">
       <h1 className="text-3xl font-bold text-center mb-10">Games</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 px-4">
-        {gameData.map((game) => (
-          <Link href={`/games/${game.game.id}`} key={game.id}>
-            <div className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
+        {gameData.map((game) => {
+          // Assuming the structure of your game object is correct
+          const homeScore = game.scores.home.total;
+          const awayScore = game.scores.away.total;
+          const isHomeWinner = homeScore > awayScore;
+
+          return (
+            <Link
+              className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100"
+              href={`/games/${game.game.id}`}
+              key={game.id}
+            >
               <div className="flex justify-between items-center mb-4">
                 <img
                   src={game.teams.home.logo}
                   alt={game.teams.home.name}
                   className="h-16 w-16 mr-2"
                 />
-                <span className="text-lg font-bold">{game.teams.home.name}</span>
+                <span className="text-lg font-bold">
+                  {game.teams.home.name}
+                </span>
+                <span
+                  className={`text-lg font-bold ${
+                    isHomeWinner ? "text-green-500" : "text-red-500"
+                  }`}
+                >
+                  {homeScore}
+                </span>
               </div>
               <div className="flex justify-between items-center mb-4">
                 <img
@@ -26,15 +44,24 @@ export default async function Page() {
                   alt={game.teams.away.name}
                   className="h-16 w-16 mr-2"
                 />
-                <span className="text-lg font-bold">{game.teams.away.name}</span>
+                <span className="text-lg font-bold">
+                  {game.teams.away.name}
+                </span>
+                <span
+                  className={`text-lg font-bold ${
+                    isHomeWinner ? "text-red-500" : "text-green-500"
+                  }`}
+                >
+                  {awayScore}
+                </span>
               </div>
               <div className="flex justify-between">
                 <span className="text-gray-600">{game.date}</span>
                 <span className="text-gray-600">{game.time}</span>
               </div>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
